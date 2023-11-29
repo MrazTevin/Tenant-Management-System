74384ad77b0e19e1c10c31f7306958421be0afb4
MrazTevin <millatevin@gmail.com>
2023-11-29 17:07:20 +0300

    m: add the image links

--- src/property_data.js
+++ src/property_data.js
@@ -3,31 +3,31 @@ const propertyListings = [
       type: 'Apartment',
       bedrooms: 2,
       status: 'For Sale',
-      image_url: 'https://example.com/image1.jpg',
+      image_url: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/55458458.jpg?k=9af4444a86264c87f4a7c2c529edb89fa80c8c4d1722963432ad34a2a2912c77&o=&hp=1',
     },
     {
       type: 'House',
       bedrooms: 3,
       status: 'For Rent',
-      image_url: 'https://example.com/image2.jpg',
+      image_url: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/200611389.jpg?k=ecb999dc4279f0eb6948c092d48fa2b902640bebc900e7952ed7332bfff3ed23&o=&hp=1',
     },
     {
       type: 'Condo',
       bedrooms: 1,
       status: 'For Sale',
-      image_url: 'https://example.com/image3.jpg',
+      image_url: 'https://media-cdn.tripadvisor.com/media/vr-splice-j/07/c3/2d/41.jpg',
     },
     {
       type: 'Townhouse',
       bedrooms: 4,
       status: 'For Rent',
-      image_url: 'https://example.com/image4.jpg',
+      image_url: 'https://mlcphwjm0ifi.i.optimole.com/cb:2I7o.66bd/w:auto/h:auto/q:mauto/f:best/https://commercialpropertykenya.com/wp-content/uploads/2022/02/Pyramid-Ridge.jpg',
     },
     {
       type: 'Studio',
       bedrooms: 0,
       status: 'For Sale',
-      image_url: 'https://example.com/image5.jpg',
+      image_url: 'https://pictures-kenya.jijistatic.com/52409236_MzAwLTQwMC03MGFhMTcyODM3.webp',
     },
   ];
   
