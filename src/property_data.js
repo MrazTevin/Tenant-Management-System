cc2e11942434f85776d0d4ffb848748582c59ee3
MrazTevin <millatevin@gmail.com>
2023-11-29 16:46:24 +0300

    m: create static list of properties

---
+++ src/property_data.js
@@ -0,0 +1,35 @@
+const propertyListings = [
+    {
+      type: 'Apartment',
+      bedrooms: 2,
+      status: 'For Sale',
+      image_url: 'https://example.com/image1.jpg',
+    },
+    {
+      type: 'House',
+      bedrooms: 3,
+      status: 'For Rent',
+      image_url: 'https://example.com/image2.jpg',
+    },
+    {
+      type: 'Condo',
+      bedrooms: 1,
+      status: 'For Sale',
+      image_url: 'https://example.com/image3.jpg',
+    },
+    {
+      type: 'Townhouse',
+      bedrooms: 4,
+      status: 'For Rent',
+      image_url: 'https://example.com/image4.jpg',
+    },
+    {
+      type: 'Studio',
+      bedrooms: 0,
+      status: 'For Sale',
+      image_url: 'https://example.com/image5.jpg',
+    },
+  ];
+  
+  export default propertyListings;
+  
